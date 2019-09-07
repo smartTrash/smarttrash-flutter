758afae0e9c7eb57a46eacd09f79b51fd74959d6
chadaporn29797 <chadaporn.ne.60@ubu.ac.th>
2019-09-07 12:12:45 +0700

    commit

--- smarttrash/lib/hommy.dart
+++ smarttrash/lib/hommy.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:smarttrash/style/theme.dart' as Theme;
 import 'login_page.dart';
 import 'home.dart';
-import 'hommy.dart';
+import 'trach.dart';
 
 class HomePage2 extends StatefulWidget {
   @override
@@ -72,56 +72,7 @@ class MainContent extends StatelessWidget {
               new Stack(
               alignment: Alignment.center,
               children: <Widget>[
-                new Container(
-                  height: 60.0,
-                  width: 60.0,
-                  decoration: new BoxDecoration(
-                      borderRadius: new BorderRadius.circular(60.0),
-                      color: Color(0xFF18D191)),
-                  child: new Icon(
-                    Icons.home,
-                    color: Colors.white,
-                  ),
-                ),
-                new Container(
-                  margin: new EdgeInsets.only(right: 50.0, top: 50.0),
-                  height: 60.0,
-                  width: 60.0,
-                  decoration: new BoxDecoration(
-                      borderRadius: new BorderRadius.circular(50.0),
-                      color: Color(0xFFFC6A7F)),
-                  child: new Icon(
-                    Icons.delete,
-                    color: Colors.white,
-                  ),
-                ),
-                new Container(
-                  margin: new EdgeInsets.only(left: 30.0, top: 50.0),
-                  height: 60.0,
-                  width: 60.0,
-                  decoration: new BoxDecoration(
-                      borderRadius: new BorderRadius.circular(50.0),
-                      color: Color(0xFFFFCE56)),
-                  child: new Icon(
-                    Icons.local_car_wash,
-                    color: Colors.white,
-                  ),
-                ),
-                new Container(
-                  margin: new EdgeInsets.only(left: 90.0, top: 40.0),
-                  height: 60.0,
-                  width: 60.0,
-                  decoration: new BoxDecoration(
-                      borderRadius: new BorderRadius.circular(50.0),
-                      color: Color(0xFF45E0EC)),
-                  child: new Icon(
-                    Icons.place,
-                    color: Colors.white,
-                  ),
-                )
-              ],
-            ),
-              Row(
+                Row(
                 children: <Widget>[
                   new Text(
                     "หน้าหลัก",
@@ -132,6 +83,9 @@ class MainContent extends StatelessWidget {
                   ),
                 ],
               ),
+              ],
+            ),
+              
               new SizedBox(
                 height: 20.0,
               ),
@@ -141,6 +95,12 @@ class MainContent extends StatelessWidget {
                           child: Padding(
                             padding:
                                 const EdgeInsets.only(top: 2.5, right: 2.5),
+                            child: GestureDetector(
+                              onTap: () {
+                                Navigator.push(context, MaterialPageRoute(
+                                  builder: (context) => Trachpage(),
+                                ));
+                              },
                             child: new Container(
                               height: 60.0,
                               decoration: new BoxDecoration(
@@ -162,6 +122,7 @@ class MainContent extends StatelessWidget {
                               ),
                             ),
                           ),
+                        ),
                         ),
                 ],
               ),
@@ -201,7 +162,7 @@ class MainContent extends StatelessWidget {
               new SizedBox(
                 height: 10.0,
               ),
-             Row(
+              Row(
                 children: <Widget>[
                   Expanded(
                           child: Padding(
@@ -223,6 +184,39 @@ class MainContent extends StatelessWidget {
                                     ),
                                   ),
                                   new Text('สถานที่',
+                                      style: new TextStyle(color: Colors.white))
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                ],
+              ),
+              new SizedBox(
+                height: 10.0,
+              ),
+             Row(
+                children: <Widget>[
+                  Expanded(
+                          child: Padding(
+                            padding:
+                                const EdgeInsets.only(top: 2.5, right: 2.5),
+                            child: new Container(
+                              height: 60.0,
+                              decoration: new BoxDecoration(
+                                  color: Color(0xFFBA68C8),
+                                  borderRadius: new BorderRadius.circular(5.0)),
+                              child: new Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  Padding(
+                                    padding: const EdgeInsets.only(right: 8.0),
+                                    child: new Icon(
+                                      Icons.equalizer,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                  new Text('สถิติ',
                                       style: new TextStyle(color: Colors.white))
                                 ],
                               ),
@@ -264,6 +258,7 @@ class MainContent extends StatelessWidget {
                         ),
                 ],
               ),
+              
             ],
           )),
         )
---
+++ smarttrash/lib/trach.dart
@@ -0,0 +1,158 @@
+import 'package:flutter/material.dart';
+import 'package:smarttrash/style/theme.dart' as Theme;
+import 'login_page.dart';
+import 'home.dart';
+import 'trach.dart';
+
+class Trachpage extends StatefulWidget {
+  @override
+  _TrachpageState createState() => new _TrachpageState();
+}
+
+class _TrachpageState extends State<Trachpage> {
+  int _bottomNavIndex=0;
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      bottomNavigationBar: new BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        fixedColor:Theme.Colors.loginGradientStart,
+         currentIndex: _bottomNavIndex,
+         onTap: (int index){
+          setState((){
+            _bottomNavIndex = index;
+            
+          });
+         },
+         
+        items: [
+          new BottomNavigationBarItem(
+            title: new Text(''),
+             icon: new Icon(Icons.home)
+          ),
+          new BottomNavigationBarItem(
+            title: new Text(''),
+             icon: new Icon(Icons.local_offer)
+          ),
+          new BottomNavigationBarItem(
+            title: new Text(''),
+             icon: new Icon(Icons.message)
+          ),
+          new BottomNavigationBarItem(
+            title: new Text(''),
+             icon: new Icon(Icons.notifications)
+          )
+
+        ],
+      ),
+      appBar: new AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+          iconTheme: new IconThemeData(color: Color(0xFF18D191))),
+      body: MainContent(
+        
+      ),
+    );
+  }
+}
+
+class MainContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new ListView(
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15.0),
+          child: new Container(
+              child: new Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  new Text(
+                    "ถังขยะ",
+                    style: new TextStyle(
+                      fontSize: 30.0,
+                    ),
+                    textAlign: TextAlign.left,
+                  ),
+                ],
+              ),
+              new SizedBox(
+                height: 20.0,
+              ),
+              Row(
+                children: <Widget>[
+                  new Expanded(
+                      child: Padding(
+                    padding: const EdgeInsets.only(right: 5.0),
+                    child: new Container(
+                      height: 100.0,
+                      decoration: new BoxDecoration(
+                          borderRadius: new BorderRadius.circular(100.0),
+                          color: Color(0xFFFD7384)),
+                      child: new Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          new Icon(
+                            Icons.delete,
+                            color: Colors.white,
+                          ),
+                          new Text("ขยะเต็ม",
+                              style: new TextStyle(color: Colors.white))
+                        ],
+                      ),
+                    ),
+                  )),
+                  new Expanded(
+                      child: Padding(
+                    padding: const EdgeInsets.only(right: 5.0),
+                    child: new Container(
+                      height: 100.0,
+                      decoration: new BoxDecoration(
+                          borderRadius: new BorderRadius.circular(100.0),
+                          color: Color(0XFFF1B069)),
+                      child: new Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          new Icon(
+                            Icons.delete,
+                            color: Colors.white,
+                          ),
+                          new Text("ขยะครึ่งถัง",
+                              style: new TextStyle(color: Colors.white))
+                        ],
+                      ),
+                    ),
+                  )),
+                  new Expanded(
+                      child: Padding(
+                    padding: const EdgeInsets.only(right: 5.0),
+                    child: new Container(
+                      height: 100.0,
+                      decoration: new BoxDecoration(
+                          borderRadius: new BorderRadius.circular(100.0),
+                          color: Color(0XFF53CEDB)),
+                      child: new Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          new Icon(
+                            Icons.delete,
+                            color: Colors.white,
+                          ),
+                          new Text("ขยะน้อย",
+                              style: new TextStyle(color: Colors.white))
+                        ],
+                      ),
+                    ),
+                  )),
+                  
+                ],
+              ),
+              
+            ],
+          )),
+        )
+      ],
+    );
+  }
+}
